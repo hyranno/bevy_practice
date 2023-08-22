5738348618eb1b87cd1be6cb8ba10e1351887f00
hyranno <hyranno4pub@gmail.com>
2023-08-22 13:33:23 +0900

    systems donot use states directly

--- src/character_control/locomotion_system.rs
+++ src/character_control/locomotion_system.rs
@@ -7,51 +7,76 @@ use crate::cascade_input::{
     axis::{PositionalInput, RotationalInput}
 };
 
-use super::{grounded_states::*, Rotation, AttachedInput, HeadAttitude, Locomotion, Head};
+use super::{Rotation, AttachedInput, HeadAttitude, Locomotion, Head};
 
 
 pub struct LocomotionSystemPlugin;
 impl Plugin for LocomotionSystemPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_systems(Update, timeout)
             .add_systems(Update, (jump_up, character_rotation, head_rotation).after(CascadeInputSet::Flush))
-            .add_systems(Update, (grounded_locomotion, airborne_locomotion).after(character_rotation))
+            .add_systems(Update, (basic_locomotion, airborne_locomotion).after(character_rotation))
         ;
     }
 }
 
 
-pub fn jump_up (
+#[derive(Component, Clone, Copy)]
+pub struct Timeout {
+    pub duration: f32,
+    pub elapsed_time: f32,
+}
+pub fn timeout (
     mut commands: Commands,
-    mut states: Query<(Entity, &mut JumpingUp, &Parent), With<GroundedStateMachine>>,
-    mut velocities: Query<&mut Velocity>,
+    mut params: Query<(Entity, &mut Timeout)>,
     time: Res<Time>,
 ) {
-    for (state_machine, mut state, parent) in states.iter_mut() {
-        let Ok(mut velocity) = velocities.get_mut(parent.get()) else {
-            warn!("Parent does not have velocity!");
-            continue;
-        };
-        let target_direction = state.target_velocity.normalize();
-        let speed_diff = state.target_velocity.length() - velocity.linvel.dot(target_direction);
-        let linvel = velocity.linvel + speed_diff.clamp(0.0, state.max_acceleration) * target_direction;
-        // avoid false change detection
-        if velocity.linvel != linvel {
-            velocity.linvel = linvel;
-        }
-        // done after duration
-        state.elapsed_time += time.delta_seconds();
-        if state.duration < state.elapsed_time {
+    let delta = time.delta_seconds();
+    for (state_machine, mut param) in params.iter_mut() {
+        param.elapsed_time += delta;
+        if param.duration < param.elapsed_time {
             commands.entity(state_machine).insert(Done::Success);
         }
     }
 }
 
+#[derive(Component, Clone, Copy)]
+pub struct JumpUp {
+    pub target_velocity: Vec3,
+    pub max_acceleration: f32,
+}
+pub fn jump_up (
+    mut params: Query<(&mut JumpUp, &Parent)>,
+    mut velocities: Query<&mut Velocity>,
+) {
+    for (param, parent) in params.iter_mut() {
+        let Ok(mut velocity) = velocities.get_mut(parent.get()) else {
+            warn!("Parent does not have velocity!");
+            continue;
+        };
+        let target_direction = param.target_velocity.normalize();
+        let speed_diff = param.target_velocity.length() - velocity.linvel.dot(target_direction);
+        let linvel = velocity.linvel + speed_diff.clamp(0.0, param.max_acceleration) * target_direction;
+        // avoid false change detection
+        if velocity.linvel != linvel {
+            velocity.linvel = linvel;
+        }
+    }
+}
+
+#[derive(Component, Clone, Copy)]
+pub struct CharacterRotation;
 pub fn character_rotation(
     mut characters: Query<(&mut Transform, &AttachedInput<Rotation>)>,
+    params: Query<(&CharacterRotation, &Parent)>,
     rotational_inputs: Query<&RotationalInput>,
 ) {
-    for (mut transform, input) in characters.iter_mut() {
+    for (_param, parent) in params.iter() {
+        let Ok((mut transform, input)) = characters.get_mut(parent.get()) else {
+            warn!("Entity not found!");
+            continue;
+        };
         // rotation
         if let Ok(rotation) = rotational_inputs.get(**input) {
             // avoid false change detection
@@ -62,12 +87,19 @@ pub fn character_rotation(
     }
 }
 
+#[derive(Component, Clone, Copy)]
+pub struct HeadRotation;
 pub fn head_rotation (
     characters: Query<&AttachedInput<HeadAttitude>, With<Children>>,
     mut heads: Query<(&mut Transform, &Parent), With<Head>>,
+    params: Query<(&HeadRotation, &Parent)>,
     rotational_inputs: Query<&RotationalInput>,
 ) {
-    for (mut transform, parent) in heads.iter_mut() {
+    for (_param, parent) in params.iter() {
+        let Some((mut transform, parent)) = heads.iter_mut().filter(|(_, character)| parent.get() == character.get()).next() else {
+            warn!("Head not found!");
+            continue;
+        };
         let Ok(inputs) = characters.get(parent.get()) else {
             warn!("Entity not found!");
             continue;
@@ -83,13 +115,18 @@ pub fn head_rotation (
     }
 }
 
-pub fn grounded_locomotion (
+#[derive(Component, Clone, Copy)]
+pub struct BasicLocomotion {
+    pub speed: f32,
+    pub max_acceleration: f32,
+}
+pub fn basic_locomotion (
     mut characters: Query<(&GlobalTransform, &mut Velocity, &AttachedInput<Locomotion>)>,
-    states: Query<&Parent, Or<(With<Grounded>, With<JumpingUp>)>>,
+    params: Query<(&BasicLocomotion, &Parent)>,
     positional_inputs: Query<&PositionalInput>,
 ) {
-    for character in states.iter() {
-        let Ok((transform, mut velocity, input)) = characters.get_mut(character.get()) else {
+    for (param, parent) in params.iter() {
+        let Ok((transform, mut velocity, input)) = characters.get_mut(parent.get()) else {
             warn!("Entity not found!");
             continue;
         };
@@ -97,15 +134,12 @@ pub fn grounded_locomotion (
             warn!("Entity not found!");
             continue;
         };
-        // TODO: parameterize
-        let movement_speed = 2.0;
-        let max_acceleration = 2.0;
         let (_scale, rotation, _translation) = transform.to_scale_rotation_translation();
-        let target_velocity = movement_speed * rotation.mul_vec3(**locomotion);
+        let target_velocity = param.speed * rotation.mul_vec3(**locomotion);
         if 0.0 < target_velocity.length() {
             let target_direction = target_velocity.normalize();
             let speed_diff = target_velocity.length() - velocity.linvel.dot(target_direction);
-            let linvel = velocity.linvel + speed_diff.clamp(0.0, max_acceleration) * target_direction;
+            let linvel = velocity.linvel + speed_diff.clamp(0.0, param.max_acceleration) * target_direction;
             // avoid false change detection
             if velocity.linvel != linvel {
                 velocity.linvel = linvel;
@@ -114,13 +148,18 @@ pub fn grounded_locomotion (
     }
 }
 
+#[derive(Component, Clone, Copy)]
+pub struct AirborneLocomotion {
+    pub speed: f32,
+    pub max_acceleration: f32,
+}
 pub fn airborne_locomotion (
     mut characters: Query<(&GlobalTransform, &mut Velocity, &AttachedInput<Locomotion>)>,
-    states: Query<&Parent, With<Airborne>>,
+    params: Query<(&AirborneLocomotion, &Parent)>,
     positional_inputs: Query<&PositionalInput>,
 ) {
-    for character in states.iter() {
-        let Ok((transform, mut velocity, input)) = characters.get_mut(character.get()) else {
+    for (param, parent) in params.iter() {
+        let Ok((transform, mut velocity, input)) = characters.get_mut(parent.get()) else {
             warn!("Entity not found!");
             continue;
         };
@@ -129,16 +168,13 @@ pub fn airborne_locomotion (
             continue;
         };
         // This formula intentionally enables circle-jump like infinite speed-up
-        // TODO: parameterize
-        let max_acceleration = 0.02;
-        let max_speed = 0.6;
         let (_scale, rotation, _translation) = transform.to_scale_rotation_translation();
         let locomotion_global = rotation.mul_vec3(**locomotion);
-        let target = max_acceleration * Vec2::new(locomotion_global.x, locomotion_global.z);    // xz() swizzling not found in Bevy
+        let target = param.max_acceleration * Vec2::new(locomotion_global.x, locomotion_global.z);    // xz() swizzling not found in Bevy
         if 0.0 < target.length() {
             let horizontal_velocity = Vec2::new(velocity.linvel.x, velocity.linvel.z);
             let target_direction = target.normalize();
-            let k = (horizontal_velocity.dot(target_direction) / max_speed).clamp(0.0, 1.0);
+            let k = (horizontal_velocity.dot(target_direction) / param.speed).clamp(0.0, 1.0);
             let acceleration = target * if 0.0 < k {
                 (1.0 - k) + k*horizontal_velocity.normalize().dot(target_direction)/2.0
             } else {
